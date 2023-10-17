888a72cdb4e6185138c58eb02a7bc048c0c88951
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-17 07:57:46 -0500

    clase Futbolista

---
+++ src/test/java/futbol/Futbolista.java
@@ -0,0 +1,53 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Object> {
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	public Futbolista(String nombre, int edad, String pocision) {
+		this.setNombre(nombre);
+		this.setEdad(edad);
+		this.posicion = pocision;
+	}
+	
+	public Futbolista() {
+		this("Maradona", 30, "delantero");
+	}
+	@Override
+	public String toString() {
+		String texto= "El futbolista" +nombre+ "tiene" +edad+ ", y juega de "+ posicion;
+		return texto;
+	}
+	
+	public boolean equals(Futbolista f) {
+		if (this == f) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public abstract boolean jugarConLasManos();
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+}
